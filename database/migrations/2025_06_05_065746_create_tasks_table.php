9437d4e8bfe2ddd4e74f5696b4ad88f237d31e25
oogane-kaito <oogane.kaito@trans-cosmos.co.jp>
2025-06-10 15:55:09 +0900

    完成

--- database/migrations/2025_06_05_065746_create_tasks_table.php
+++ database/migrations/2025_06_05_065746_create_tasks_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string("content");
+            $table->char("status",10);
+
             $table->timestamps();
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
